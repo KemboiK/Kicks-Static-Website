a96fd4a4b2b535ab73fc474ffa87b13d73c85342
KemboiK <kipropkelvin374@gmail.com>
2024-02-11 15:11:15 +0300

    Fixed price tags

--- shop.js
+++ shop.js
@@ -1,8 +1,8 @@
 // Sample array of products
 const products = [
-    { name: 'HP', price: '$20', category: 'Laptops', image: 'C:\Users\ADMIN\Downloads\laptop1.jpg' },
-    { name: 'HP spectre', price: '$30', category: 'Laptops', image: 'C:\Users\ADMIN\Downloads\laptop2.webp' },
-    { name: 'Macbook', price: '$25', category: 'Laptops', image: 'C:\Users\ADMIN\Downloads\laptop3.avif' }
+    { name: 'HP', price: '$2000', category: 'Laptops', image: 'C:\Users\ADMIN\Downloads\laptop1.jpg' },
+    { name: 'HP spectre', price: '$1500', category: 'Laptops', image: 'C:\Users\ADMIN\Downloads\laptop2.webp' },
+    { name: 'Macbook', price: '$3000', category: 'Laptops', image: 'C:\Users\ADMIN\Downloads\laptop3.avif' }
 ];
 
 const content = document.querySelector('.content');
